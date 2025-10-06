83db742190a535f9a54e8a59775fdc259049a8bc
tasmiya0713 <niloshaik29@eng.rizvi.edu.in>
2025-10-06 16:14:30 +0530

    Experiment1B.c

---
+++ Experiment1B.c
@@ -0,0 +1,29 @@
+/* Name :- TASMIYA HASEEN SHAIKH
+UIN: -251C020
+Class: -CIVIL
+DIV:-B
+__*/
+#include<stdio.h>
+int main()
+{
+int physics;
+int chemistry;
+int math;
+printf(" Enter physics marks=");
+scanf ("%d", &physics) ;
+
+printf("Enter Chemistry marks=");
+scanf ("%d", &chemistry) ;
+
+printf ("Enter Math marks=");
+scanf ("%d", &math);
+
+int sum;
+float avg;
+sum=physics + chemistry + math);
+avg=(float) sum/3;
+printf("\n sum is %d \n avg is of, sum, avg");
+(avg>=50)? printf("\n person is eligible for admission): printf("\n person is not eligible for admission);
+return 0;
+}
+}
